210aa4e2ec8af6b355437ccf85d69ad832f61f6c
DenAnisimov <den.anisimov.2206@mail.ru>
2024-07-06 01:34:20 +0300

    Тесты

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyHashMap<String, Integer> myHashMap = new MyHashMap<>();
+        myHashMap.put("a", 1);
+        myHashMap.put("b", 2);
+        myHashMap.put("c", 3);
+        myHashMap.put("d", 4);
+        myHashMap.put("e", 5);
+        myHashMap.put("f", 6);
+        myHashMap.put("g", 7);
+        myHashMap.put("h", 8);
+        myHashMap.put("i", 9);
+        myHashMap.put("j", 10);
+        myHashMap.put("k", 11);
+        myHashMap.put("l", 12);
+        myHashMap.put("m", 13);
+        myHashMap.put("n", 14);
+        myHashMap.put("o", 15);
+        myHashMap.put("p", 16);
+        System.out.println(myHashMap.get("a"));
     }
 }
